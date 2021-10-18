1d6ed52f905aabda2b5fc71cd12ba166579d6e14
RachelRebecca <rachelrebecca67@gmail.com>
2021-10-17 21:25:09 -0400

    client class and threads updated according to pseudocode

---
+++ src/ClientThread2.java
@@ -0,0 +1,14 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+public class ClientThread2 extends Thread
+{
+    public ClientThread2(String fullID, Socket clientSocket, Object socket_LOCK,
+                         ArrayList<String> unfinishedList, Object unfinished_LOCK)
+    {
+        // wait for acknowledgement that job has been received by master - use ClientSocket and socket_LOCK
+        System.out.println("Message " + fullID + "has been sent.");
+        unfinishedList.add(fullID); //update for Client as well? Use LOCK
+    }
+}
