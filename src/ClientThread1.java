a88c6c5fb1090aa9fc99c8a7f33c81d5026c345e
RachelRebecca <rachelrebecca67@gmail.com>
2021-10-20 15:39:01 -0400

    updated fullId

--- src/ClientThread1.java
+++ src/ClientThread1.java
@@ -7,7 +7,7 @@ public class ClientThread1 extends Thread
     public ClientThread1(String clientLetter, String jobType, int id, Object id_LOCK, Socket clientSocket, Object socket_LOCk,
                          ArrayList<String> unreceivedList, Object unreceived_LOCK)
     {
-        String fullID = clientLetter + ", " + jobType + ", " + id;
+        String fullID = clientLetter + "." + jobType + "." + id;
 
         // id++ - somehow that has to affect the other ID. Do this using ID_LOCK
         //send fullID to master -- in a try/catch?
