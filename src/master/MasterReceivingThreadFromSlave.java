24975e2ed81bd3cf0f990f8c4d1d9ff697414646
chaya <chayaz.haor@gmail.com>
2021-11-25 15:36:31 -0500

    master receiving thread from slave done. Questions re master: FIFO or priority?

--- src/master/MasterReceivingThreadFromSlave.java
+++ src/master/MasterReceivingThreadFromSlave.java
@@ -1,6 +1,53 @@
 package master;
 
-public class MasterReceivingThreadFromSlave
-{
+import resources.Done;
+import resources.Job;
+import resources.JobStatuses;
 
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class MasterReceivingThreadFromSlave extends Thread
+{
+    private Socket slaveSocket;
+    private Done done;
+    private ArrayList<Job> unfinishedList;
+    private final Object unfinishedList_LOCK;
+
+
+    public MasterReceivingThreadFromSlave(Socket slaveSocket, Done done, ArrayList<Job> unfinishedList, Object unfinishedList_LOCK)
+    {
+        this.slaveSocket = slaveSocket;
+        this.unfinishedList = unfinishedList;
+        this.unfinishedList_LOCK = unfinishedList_LOCK;
+        this.done = done;
+    }
+
+    @Override
+    public void run()
+    {
+        try (// stream to read object response from server
+             ObjectInputStream objectInputStream = new ObjectInputStream(slaveSocket.getInputStream()))
+        {
+            Job receivedJob;
+            while ((receivedJob = (Job) objectInputStream.readObject()) != null)
+            {
+                if (receivedJob.getStatus() == JobStatuses.FINISHED_SEND_TO_MASTER)
+                {
+                    receivedJob.setStatus(JobStatuses.ACK_MASTER_RECEIVED_FINISHED);
+                    synchronized (unfinishedList_LOCK)
+                    {
+                        unfinishedList.add(receivedJob);
+                    }
+                    System.out.println("Got a completed job from slave: " + receivedJob.getType() + receivedJob.getId());
+                }
+            }
+
+
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 }
