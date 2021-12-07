c5b192b45aeb3eb0163e341e0d005c75b9cf1d9a
RachelRebecca <rachelrebecca67@gmail.com>
2021-12-07 14:21:07 -0500

    added some print statements

--- src/master/MasterSendingThreadToSlave.java
+++ src/master/MasterSendingThreadToSlave.java
@@ -53,10 +53,12 @@ public class MasterSendingThreadToSlave extends Thread
                     synchronized (unfinishedJobs_LOCK)
                     {
                         currJob = unfinishedJobs.get(0);
+                        System.out.println("current job [from master sending] is " + currJob);
                         unfinishedJobs.remove(0);
                     }
 
                     SlaveTypes slave = LoadBalance.loadBalance(timeTrackerForSlaveA, timeTrackerForSlaveB, currJob);
+                    System.out.println("Sending to " + slave.name());
                     if (slave.equals(SlaveTypes.A))
                     {
                         objectOutputStreamSlaveA.writeObject(currJob);
@@ -82,6 +84,7 @@ public class MasterSendingThreadToSlave extends Thread
         System.out.println("exiting thread.");
     }
 
+    //shouldn't this be SLAVETYPE not jobtype?
     public static void updateTimeTracker(Job currJob, TimeTrackerForSlave timeTrackerForSlave)
     {
         if (currJob.getType().equals(JobTypes.A))
